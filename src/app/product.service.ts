df29cc9b67bcf32b13d2d0cd8d8c448a46f60e84
Garrett Bergeson <garrett.bergeson@progleasing.com>
2021-09-20 12:00:03 -0600

    updates

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -14,12 +14,13 @@ export class ProductService {
   constructor(private _http: Http) { }
 
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) =>
+    return this._http.get(this._albumUrl).map(response =>
     <Album>response.json());
   }
 
   getProducts(): Observable<Product[]> {
-    return this._http.get(this._productsUrl).map((response) =>
+    var products = this._http.get(this._productsUrl).map(response =>
     <Product[]>response.json());
+    return products;
   }
 }
